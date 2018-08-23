b8f026392b4135a5c562d14c8fedd48435f63308
tibbs001 <sheri.tibbs@duke.edu>
2018-08-22 22:26:38 -0400

    aact-742:  Add 4 new IPD columns

--- db/schema.rb
+++ db/schema.rb
@@ -581,6 +581,10 @@ ActiveRecord::Schema.define(version: 20161212174016) do
     t.boolean  "is_us_export"
     t.string   "biospec_retention"
     t.text     "biospec_description"
+    t.string   "ipd_info_type"
+    t.string   "ipd_time_frame"
+    t.string   "ipd_access_criteria"
+    t.string   "ipd_url"
     t.string   "plan_to_share_ipd"
     t.string   "plan_to_share_ipd_description"
     t.datetime "created_at",                          null: false
