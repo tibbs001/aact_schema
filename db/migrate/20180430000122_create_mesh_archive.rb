af63bb431ae9927b2c4450362e53bac5baf9fca6
tibbs001 <sheri.tibbs@duke.edu>
2018-12-21 14:24:20 -0500

    Add migrations for projects: CDEK Standard Orgs, Tag Nephrology & Results Reporting.

---
+++ db/migrate/20180430000122_create_mesh_archive.rb
@@ -0,0 +1,46 @@
+class CreateMeshArchive < ActiveRecord::Migration
+
+  def up
+
+    execute "CREATE SCHEMA mesh_archive;"
+    execute "GRANT USAGE ON SCHEMA mesh_archive to read_only;"
+    execute "GRANT SELECT ON ALL TABLES IN SCHEMA mesh_archive TO read_only;"
+    create_table 'mesh_archive.y2010_mesh_terms' do |t|
+      t.string  'qualifier'
+      t.string  'tree_number'
+      t.string  'description'
+      t.string  'mesh_term'
+      t.string  'downcase_mesh_term'
+    end
+
+    create_table 'mesh_archive.y2016_mesh_terms' do |t|
+      t.string  'qualifier'
+      t.string  'tree_number'
+      t.string  'description'
+      t.string  'mesh_term'
+      t.string  'downcase_mesh_term'
+    end
+
+    create_table 'mesh_archive.y2016_mesh_headings' do |t|
+      t.string  'qualifier'
+      t.string  'heading'
+      t.string  'subcategory'
+    end
+
+    add_index 'mesh_archive.y2010_mesh_terms', :qualifier
+    add_index 'mesh_archive.y2010_mesh_terms', :description
+    add_index 'mesh_archive.y2010_mesh_terms', :mesh_term
+    add_index 'mesh_archive.y2010_mesh_terms', :downcase_mesh_term
+
+    add_index 'mesh_archive.y2016_mesh_terms', :qualifier
+    add_index 'mesh_archive.y2016_mesh_terms', :description
+    add_index 'mesh_archive.y2016_mesh_terms', :mesh_term
+    add_index 'mesh_archive.y2016_mesh_terms', :downcase_mesh_term
+    add_index 'mesh_archive.y2016_mesh_headings', :qualifier
+
+  end
+
+  def down
+    execute "DROP SCHEMA mesh_archive CASCADE;"
+  end
+end
---
+++ db/migrate/20180719000122_create_proj_tag_tables.rb
@@ -0,0 +1,23 @@
+class CreateProjTagTables < ActiveRecord::Migration
+
+  def up
+
+    execute "CREATE SCHEMA proj_tag;"
+    execute "GRANT USAGE ON SCHEMA proj_tag to read_only;"
+    execute "GRANT SELECT ON ALL TABLES IN SCHEMA proj_tag TO read_only;"
+    create_table "proj_tag.tagged_terms" do |t|
+      t.integer 'project_id'
+      t.string  'identifier'
+      t.string  'tag'
+      t.string  'term'
+      t.string  'year'
+      t.string  'term_type'
+    end
+
+  end
+
+  def down
+    execute "DROP SCHEMA proj_tag CASCADE;"
+  end
+
+end
