60181a1c181d5abea24c70fccc24358ac05a2e99
tibbs001 <sheri.tibbs@duke.edu>
2019-03-28 13:22:12 -0400

    aact-930:  Upgrade db modeler to version 0.9.2-alpha1  (previous versions didn’t work with PostgreSQL 11.1)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181108000122) do
+ActiveRecord::Schema.define(version: 20190307000122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
